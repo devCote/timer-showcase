46b1be2efa8cb0da0e7a4e92635ab79a590de449
DmitriyBakhanenko <bahanenko@gmail.com>
2022-05-19 19:43:30 +0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import { ChakraProvider } from "@chakra-ui/react"
+
+const Website = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
